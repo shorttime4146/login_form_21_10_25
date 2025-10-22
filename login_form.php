1c520483f2271c8af6f3ad71b77a3765fa327118
shorttime4146 <shorttimememoryloss4146@gmail.com>
2025-10-22 00:21:11 -0800

    login form

---
+++ login_form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head></head>
+    <body>
+        <form>
+            <input type="text" name="username" placeholder="Username" value=""></br>
+            <input type="password" name="password" placeholder="Password" value=""></br>
+            <input type="password" name="forget_password" placeholder="Forget Password?" value=""></br></br>
+            <input type="submit" value="login">
+        </form>
+    </body>
+</html>
